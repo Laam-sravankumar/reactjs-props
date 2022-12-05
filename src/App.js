7826acf7097b2df4020432c42a78707bb058c393
Your Name <sravanlam9515@gmail.com>
2022-12-05 11:52:59 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 /*import PropsExample from './PropsExample'
 import FuncPropExample from './FuncPropExample'
-import StateExample from './StateExample'*/
-import Events from './Events'
-
+import StateExample from './StateExample'
+import Events from './Events'*/
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import NotFound from './components/NotFound'
 function Demo(){
   return(
     <div>
-        <Events/>
-        {/*
+          <BrowserRouter>
+          <Navbar/>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="*" element={<NotFound/>}/>
+              </Routes>
+          </BrowserRouter>      
+          {/*<Events/>
           <PropsExample framework="AngularJS" year="2008"/>
 				<PropsExample framework="ReactJS" year="2013"/>
 				<PropsExample framework="VueJS" year="2016"/>
